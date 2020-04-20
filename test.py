4e7560866dbd9c51c79f046cd6a9fdada2db71a6
bareboux <56490972+bareboux@users.noreply.github.com>
2020-04-20 17:17:05 +0300

    data file is now downloaded on every run

--- test.py
+++ test.py
@@ -1,3 +1,16 @@
 from pathlib import Path
+
+
+import requests
+
+r = requests.head("https://raw.githubusercontent.com/datasets/covid-19/master/data/time-series-19-covid-combined.csv")
+print(r.headers["Content-Length"])
+print(r.headers)
+print(len(open(Path.cwd() / "covid-stats" / "time-series-19-covid-combined.csv").readlines(  )))
+print((Path.cwd() / "covid-stats" / "time-series-19-covid-combined.csv").stat().st_size)
+#print(parsedate(r.headers))
+
+#df = pd.read_csv(Path.cwd() / "covid-stats" / "time-series-19-covid-combined_csv.csv", usecols = ["Date", "Country/Region", "Confirmed", "Recovered", "Deaths"], index_col=[1])
+#df = pd.read_csv(r.content, usecols = ["Date", "Country/Region", "Confirmed", "Recovered", "Deaths"], index_col=[1])
 print(Path.cwd())
 print("dsssd")
